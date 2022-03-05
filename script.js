bcd6152cac3b39d08d0d6c2453612b7c8bed8499
Ademar Batista <ademarbatista199720@gmail.com>
2022-03-05 15:16:43 -0300

    Commit Final

---
+++ script.js
@@ -0,0 +1,9 @@
+function somar() {
+    var tn1 = window.document.getElementById('txt1')
+    var tn2 = window.document.querySelector('input#txt2')
+    var res = window.document.getElementById('res')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var s = n1 + n2
+    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
+}
